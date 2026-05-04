21ff3a5843d2b2d60ed5e765b1e5ce81ca1a7e66
mansib-uddin101 <drivemansib@gmail.com>
2026-05-04 13:56:01 +0600

    vercel deploy isse fix 2

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,15 +2,20 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
+if (!process.env.AUTH_DB_URI) {
+  throw new Error("Missing AUTH_DB_URI in environment variables");
+}
+
 const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db("nextread");
 
 export const auth = betterAuth({
+    // Explicitly set the base URL
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000", 
     emailAndPassword: {
         enabled: true,
     },
     database: mongodbAdapter(db, {
-        // Optional: if you don't provide a client, database transactions won't be enabled.
         client
     }),
 });
